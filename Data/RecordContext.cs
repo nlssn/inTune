803952b84b26c1db3ed93f5e4b901c52f313f3f0
Johannes Nilsson <johannes@nilsson.im>
2021-02-03 21:52:52 +0100

    Add RecordContext

---
+++ Data/RecordContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using inTune.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace inTune.Data
+{
+    public class RecordContext:DbContext
+    {
+        // Constructor
+        public RecordContext(DbContextOptions<RecordContext> options):base(options)
+        {
+
+        }
+
+        // Create a DbSet
+        public DbSet<Record> Records { get; set; }
+    }
+}
