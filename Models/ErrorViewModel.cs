e323a74e1318b9ec9545378a27eabe336b4c30b8
Johannes Nilsson <johannes@nilsson.im>
2021-02-03 21:33:05 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace inTune.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
