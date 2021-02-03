d6d9aaf2db1c35062ef1cfca8bc5696039b3ffde
Johannes Nilsson <johannes@nilsson.im>
2021-02-03 21:53:57 +0100

    Scaffold RecordController with views

---
+++ Controllers/RecordController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using inTune.Data;
+using inTune.Models;
+
+namespace inTune.Controllers
+{
+    public class RecordController : Controller
+    {
+        private readonly RecordContext _context;
+
+        public RecordController(RecordContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Record
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Records.ToListAsync());
+        }
+
+        // GET: Record/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var record = await _context.Records
+                .FirstOrDefaultAsync(m => m.RecordId == id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+
+            return View(record);
+        }
+
+        // GET: Record/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Record/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RecordId,Artist,Title,Year,NumOfTracks")] Record record)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(record);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(record);
+        }
+
+        // GET: Record/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var record = await _context.Records.FindAsync(id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+            return View(record);
+        }
+
+        // POST: Record/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RecordId,Artist,Title,Year,NumOfTracks")] Record record)
+        {
+            if (id != record.RecordId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(record);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecordExists(record.RecordId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(record);
+        }
+
+        // GET: Record/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var record = await _context.Records
+                .FirstOrDefaultAsync(m => m.RecordId == id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+
+            return View(record);
+        }
+
+        // POST: Record/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var record = await _context.Records.FindAsync(id);
+            _context.Records.Remove(record);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecordExists(int id)
+        {
+            return _context.Records.Any(e => e.RecordId == id);
+        }
+    }
+}
