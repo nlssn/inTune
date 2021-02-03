f81b7f2eeb2ed716b6cc0ec666623ff5d7b4464f
Johannes Nilsson <johannes@nilsson.im>
2021-02-03 21:52:07 +0100

    Add Db service

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using inTune.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,11 @@ namespace inTune
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // Add DbContext service
+            services.AddDbContext<RecordContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultDbString"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
