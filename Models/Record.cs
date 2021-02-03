7e74888540501e1d2d1b27672c3a23ea0493aecb
Johannes Nilsson <johannes@nilsson.im>
2021-02-03 21:34:48 +0100

    Add Record model

---
+++ Models/Record.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inTune.Models
+{
+    public class Record
+    {
+        public int RecordId { get; set; }
+        public int Artist { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public int NumOfTracks { get; set; }
+    }
+}
