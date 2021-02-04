3628ee134e7f6bd6545a5f878e80f92224f79408
Johannes Nilsson <johannes@nilsson.im>
2021-02-04 05:56:28 +0100

    Add Artist model

---
+++ Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inTune.Models
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Record> Discography { get; set; }
+    }
+}
