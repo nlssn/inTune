cb9f84fcc37137a4b688f2d89dd356c1756e7b65
Johannes Nilsson <johannes@nilsson.im>
2021-02-04 05:57:54 +0100

    Add Artist properties

--- Models/Record.cs
+++ Models/Record.cs
@@ -7,8 +7,9 @@ namespace inTune.Models
 {
     public class Record
     {
-        public int RecordId { get; set; }
-        public int Artist { get; set; }
+        public int Id { get; set; }
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
         public string Title { get; set; }
         public string Year { get; set; }
         public int NumOfTracks { get; set; }
