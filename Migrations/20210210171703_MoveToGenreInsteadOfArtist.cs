fade78bf0f6a0ae98e6f02f4a469af82811abe41
Johannes Nilsson <johannes@nilsson.im>
2021-02-10 21:44:36 +0100

    Migrations replacing Artist with Genre

---
+++ Migrations/20210210171703_MoveToGenreInsteadOfArtist.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace inTune.Migrations
+{
+    public partial class MoveToGenreInsteadOfArtist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Records_Genre_GenreId",
+                table: "Records");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre");
+
+            migrationBuilder.RenameTable(
+                name: "Genre",
+                newName: "Genres");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreId",
+                table: "Records",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Records_Genres_GenreId",
+                table: "Records",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Records_Genres_GenreId",
+                table: "Records");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres");
+
+            migrationBuilder.RenameTable(
+                name: "Genres",
+                newName: "Genre");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreId",
+                table: "Records",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Records_Genre_GenreId",
+                table: "Records",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ Migrations/20210210173945_RemoveArtistTable.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace inTune.Migrations
+{
+    public partial class RemoveArtistTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Records_Artists_ArtistId",
+                table: "Records");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ArtistId",
+                table: "Records",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Artist",
+                table: "Records",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Records_Artists_ArtistId",
+                table: "Records",
+                column: "ArtistId",
+                principalTable: "Artists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Records_Artists_ArtistId",
+                table: "Records");
+
+            migrationBuilder.DropColumn(
+                name: "Artist",
+                table: "Records");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ArtistId",
+                table: "Records",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Records_Artists_ArtistId",
+                table: "Records",
+                column: "ArtistId",
+                principalTable: "Artists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
