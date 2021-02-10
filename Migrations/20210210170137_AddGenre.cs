959792eab8e0da8bdf3db98697c6dc35876a3fe3
Johannes Nilsson <johannes@nilsson.im>
2021-02-10 18:02:52 +0100

    Migration AddGenre

---
+++ Migrations/20210210170137_AddGenre.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace inTune.Migrations
+{
+    public partial class AddGenre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GenreId",
+                table: "Records",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Records_GenreId",
+                table: "Records",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Records_Genre_GenreId",
+                table: "Records",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Records_Genre_GenreId",
+                table: "Records");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Records_GenreId",
+                table: "Records");
+
+            migrationBuilder.DropColumn(
+                name: "GenreId",
+                table: "Records");
+        }
+    }
+}
